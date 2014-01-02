3cfb7df21b85f2969afa234e4d74a050b0555bc1
Jonathan Holloway <http://www.jonathanholloway.co.uk>
2014-01-02 15:40:49 +0000

    Initial set of archetype files

---
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+    }
+}
